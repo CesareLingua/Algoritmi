0f9033b6f3b01dcff7094a21a7cafc4f68fce1e3
“Cesare.Lingua” <mario.rossi@educ.di.unito.it>
2016-03-21 18:22:53 +0100

    Creazione scheletro Insertion sort

--- laboratorioalgoritmi-2015-16/Esercizio1/Insertionsort/InsertionSortTest.java
+++ laboratorioalgoritmi-2015-16/Esercizio1/Insertionsort/InsertionSortTest.java
@@ -1,36 +1,38 @@
 import java.util.*;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
 
 public class InsertionSortTest{
 
-	public static void test_insertion_sort_on_empty_array(){
+	public static void testInsertionSortOnEmptyArray(){
 		int[] array = {};
 		InsertionSort.insertionSort(array);
 		//assertArrayEquals( new int[]{}, InsertionSort.insertionSort(array));	
 	}
 
-	public static void test_insertion_sort_on_null_array(){
+	public static void testInsertionSortOnNullArray(){
 
 		
 	}
 
-	public static void test_insertion_sort_on_full_array(){
+	public static void testInsertionSortOnFullArray(){
 		int[] array = {5,3,10,7,2,4,6,1};
 		InsertionSort.insertionSort(array);
 
   }
 	}
 
-	public static void test_insertion_sort_on_equals_elements(){
+	public static void testInsertionSortOnEqualsElements(){
 		int[] array = {5,5,5,5,5,5,5,5};
 		InsertionSort.insertionSort(array);
 		System.out.println("OK!");
 	}
 
 	public static void main (String[] args){
-		test_insertion_sort_on_empty_array();
-		test_insertion_sort_on_null_array();
-		test_insertion_sort_on_full_array();
-		test_insertion_sort_on_equals_elements();
+		testInsertionSortOnEmptyArray();
+		testInsertionSortOnNullArray();
+		testInsertionSortOnFullArray();
+		testInsertionSortOnEqualsElements();
 		
 	}
 
