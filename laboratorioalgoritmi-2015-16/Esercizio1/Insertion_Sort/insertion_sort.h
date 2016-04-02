a66ff2c01a7bc643bb5091566cf6671065c0e8a8
“Cesare.Lingua” <cesare.lingua@edu.di.unito.it>
2016-04-02 11:37:18 +0200

    Header corretto insertionsort

---
+++ laboratorioalgoritmi-2015-16/Esercizio1/Insertion_Sort/insertion_sort.h
@@ -0,0 +1,14 @@
+#ifndef __INSERTION_SORT_H_KEIXJ4PDU3__
+#define __INSERTION_SORT_H_KEIXJ4PDU3__
+
+typedef int (*CompareFunc)(void*, void*);
+
+/*
+ * Implementa l'algoritmo di ordinamento Insertion Sort
+ * array: array da ordinare 
+ * size: taglia dell'array
+ * compare: funzione per valutare l'ordine di 2 elementi dell'arry
+ */
+void insertion_sort(void** array, int size, CompareFunc compare);
+
+#endif
